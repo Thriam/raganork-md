0acbc21b16a02bfb1db6ff25e2009ea752903c28
Thriam <96582906+Thriam@users.noreply.github.com>
2022-04-26 14:26:43 +0530

    Update fancy.js

--- plugins/fancy.js
+++ plugins/fancy.js
@@ -3,7 +3,7 @@ const {Fancy} = require('raganork-bot')
 const {MODE} = require('../config');
 var w = MODE == 'public'? false:true
 Module({pattern: 'fancy ?(.*)', fromMe: w, desc: 'Creates fancy text fonts'}, (async (message, match) => {
-if (!match[1]||!message.reply_message.message) return await message.sendReply("Reply to any message with .fancy number\n"+Fancy("example","32"))
-try { var result = Fancy(message.reply_message.message,match[1]) } catch (e) {return await message.sendReply(e.message)}
-await message.sendReply(result)
+if (!match[1]||!message.reply_message.message) return await message.sendMessage("Reply to any message with .fancy number\n"+Fancy("example","32"))
+try { var result = Fancy(message.reply_message.message,match[1]) } catch (e) {return await message.sendMessage(e.message)}
+await message.sendMessage(result)
      }))
